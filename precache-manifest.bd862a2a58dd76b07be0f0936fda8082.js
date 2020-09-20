ccc26dac3170a9c2bc1062b2269bc00541285d01
Jeff Sargent <jeff.sargent@gmail.com>
2020-09-20 16:36:12 -0400

    Updates

---
+++ precache-manifest.bd862a2a58dd76b07be0f0936fda8082.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fead8ad2b5a6d319824b006be88b689",
+    "url": "/fretboard/index.html"
+  },
+  {
+    "revision": "514bdc40d42dfb4a95c0",
+    "url": "/fretboard/static/css/main.a631c525.chunk.css"
+  },
+  {
+    "revision": "e6318c9ffcac7a440de6",
+    "url": "/fretboard/static/js/2.d2cdbbef.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/fretboard/static/js/2.d2cdbbef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514bdc40d42dfb4a95c0",
+    "url": "/fretboard/static/js/main.8e99d590.chunk.js"
+  },
+  {
+    "revision": "994a6bf9469a8f874732",
+    "url": "/fretboard/static/js/runtime-main.cad05f07.js"
+  }
+]);
